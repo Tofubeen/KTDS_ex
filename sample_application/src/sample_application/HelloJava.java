ed7dc3c4e690369d5cd157eb674c3042994cffed
Tofubeen <137252447+Tofubeen@users.noreply.github.com>
2024-07-11 16:43:37 +0900

    first commit

---
+++ sample_application/src/sample_application/HelloJava.java
@@ -0,0 +1,10 @@
+package sample_application;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" ");
+	}
+	
+}
---
+++ sample_application/src/sample_application/TypeCastingExam.java
@@ -0,0 +1,11 @@
+package sample_application;
+
+public class TypeCastingExam {
+
+	public static void main(String[] args) {
+		
+		short shortNumber = 10_000;
+		int intNumber = shortNumber;
+		
+	}
+}
